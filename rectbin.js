b918ad120b4efc69d83a836491e42b48c635fe9b
stephan mantler <step@stephanmantler.com>
2022-12-22 15:53:05 +0000

    sparse data optimisation - lazy bin initialization

--- rectbin.js
+++ rectbin.js
@@ -10,28 +10,24 @@
       var binsById = {};
       var xExtent = d3.extent(points, function(d, i){ return x.call(rectbin, d, i) ;});
       var yExtent = d3.extent(points, function(d, i){ return y.call(rectbin, d, i) ;});
-
-      d3.range(yExtent[0], yExtent[1] + dx, dy).forEach(function(Y){
-        d3.range(xExtent[0], xExtent[1] + dx, dx).forEach(function(X){
-          var py = Y / dy; 
-          var pj = trunc(py);
-          var px = X / dx;
-          var pi = trunc(px);
-          var id = pi + '-' + pj;
-          var bin = binsById[id] = [];
-          bin.i = pi;
-          bin.j = pj;
-          bin.x = pi * dx;
-          bin.y = pj * dy;
-        });
-      });
+      
       points.forEach(function(point, i) {
         var py = y.call(rectbin, point, i) / dy;
         var pj = trunc(py);
         var px = x.call(rectbin, point, i) / dx;
         var pi = trunc(px);
-
+      
         var id = pi + '-' + pj;
+        
+        if(! (id in binsById) ) {
+          binsById[id] = [];
+          var bin2 = binsById[id];
+          bin2.i = pi;
+          bin2.j = pj;
+          bin2.x = pi * dx;
+          bin2.y = pj * dy;
+        }
+        
         var bin = binsById[id];
         bin.push(point);
       });
